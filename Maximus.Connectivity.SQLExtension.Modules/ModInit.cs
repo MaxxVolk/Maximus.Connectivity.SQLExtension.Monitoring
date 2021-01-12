44b76577abc3769e10f421a53729301f6aa9e65f
Maxim Volkov <maxim.volkov@revera.co.nz>
2021-01-12 19:05:18 +1300

    Add project files.

---
+++ Maximus.Connectivity.SQLExtension.Modules/ModInit.cs
@@ -0,0 +1,37 @@
+﻿using Maximus.Library.Helpers;
+using Maximus.Library.ManagedModuleBase;
+
+using Microsoft.EnterpriseManagement.HealthService;
+
+using System;
+
+namespace Maximus.Connectivity.SQLExtension.Modules
+{
+  internal static class ModInit
+  {
+    static ModInit()
+    {
+    }
+
+    internal static char[] Separators = { ',', ';', '|' };
+
+    const string LogSourceName = "Maximus SQL Connectivity Monitoring Module";
+    const int LogBaseEventId = 6240;
+
+    static private LoggingHelper _Logger;
+    static internal LoggingHelper Logger => _Logger ?? (_Logger = new LoggingHelper(LogSourceName, LogBaseEventId, EventLoggingLevel.Informational));
+
+    internal const int evtId_SQLServerSyntheticTransactionPA = 0;
+    //internal const int evtId_ = 1;
+    //internal const int evtId_ = 2;
+    //internal const int evtId_ = 3;
+    //internal const int evtId_ = 4;
+    //internal const int evtId_ = 5;
+    //internal const int evtId_ = 6;
+
+    internal static void ModuleErrorSignalReceiver(ModuleErrorSeverity severity, ModuleErrorCriticality criticality, Exception e, string message, object callerInstance)
+    {
+      Logger.WriteException($"Internal module exception or error.\r\nMessage: {message}\r\nError Severity: {severity}\r\nError Criticality: {criticality}", e ?? new Exception("Unknown exception"), callerInstance);
+    }
+  }
+}
---
+++ Maximus.Connectivity.SQLExtension.Modules/SQLServerSyntheticTransactionPA.cs
@@ -0,0 +1,69 @@
+﻿using Maximus.Library.ManagedModuleBase;
+
+using Microsoft.EnterpriseManagement.HealthService;
+using Microsoft.EnterpriseManagement.Mom.Modules.DataItems;
+
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace Maximus.Connectivity.SQLExtension.Modules
+{
+  [MonitoringModule(ModuleType.ReadAction)]
+  [ModuleOutput(true)]
+  public class SQLServerSyntheticTransactionPA : ModuleBaseSimpleAction<PropertyBagDataItem>
+  {
+    private string FullyQualifiedDomainName, TestDisplayName;
+    private int TargetIndex;
+
+    public SQLServerSyntheticTransactionPA(ModuleHost<PropertyBagDataItem> moduleHost, XmlReader configuration, byte[] previousState) : base(moduleHost, configuration, previousState)
+    {
+    }
+
+    protected override void PreInitialize(ModuleHost<PropertyBagDataItem> moduleHost, XmlReader configuration, byte[] previousState)
+    {
+      ModInit.Logger.AddLoggingSource(GetType(), ModInit.evtId_SQLServerSyntheticTransactionPA);
+      base.PreInitialize(moduleHost, configuration, previousState);
+    }
+
+    protected override PropertyBagDataItem[] GetOutputData(DataItemBase[] inputDataItems)
+    {
+      try
+      {
+        return null;
+      }
+      catch (Exception e)
+      {
+        ModuleErrorSignalReceiver(ModuleErrorSeverity.DataLoss, ModuleErrorCriticality.ThrowAndContinue, e, $"Failed to perform SQL Server synthetic transaction for the {TestDisplayName} test object related to the {FullyQualifiedDomainName}:{TargetIndex} destination object.");
+        return null;
+      }
+    }
+
+    protected override void ModuleErrorSignalReceiver(ModuleErrorSeverity severity, ModuleErrorCriticality criticality, Exception e, string message, params object[] extaInfo)
+    {
+      ModInit.ModuleErrorSignalReceiver(severity, criticality, e, message, this);
+    }
+
+    protected override void LoadConfiguration(XmlDocument cfgDoc)
+    {
+      try
+      {
+        // base class properties
+        LoadConfigurationElement(cfgDoc, "TestDisplayName", out TestDisplayName, "<no test name provided>", false); // for logging purposes only
+        // parent class property
+        LoadConfigurationElement(cfgDoc, "FullyQualifiedDomainName", out FullyQualifiedDomainName);
+        LoadConfigurationElement(cfgDoc, "TargetIndex", out TargetIndex);
+        // specific class properties -- none
+      }
+      catch (Exception e)
+      {
+        ModuleErrorSignalReceiver(ModuleErrorSeverity.FatalError, ModuleErrorCriticality.Stop, e, "Failed to load module configuration.");
+        throw new ModuleException("Failed to load module configuration.", e);
+      }
+    }
+  }
+}
